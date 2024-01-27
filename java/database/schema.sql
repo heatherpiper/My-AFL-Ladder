5e08780740d6f4a1a6a29a53f2cf7b338954971c
Piper <heather.a.piper@gmail.com>
2024-01-27 17:16:27 -0500

    Populated database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,9 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS matches;
+DROP TABLE IF EXISTS venues;
+DROP TABLE IF EXISTS timezones;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,14 +13,11 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE matches (
-    match_id SERIAL PRIMARY KEY,
-    team1 VARCHAR(50) NOT NULL,
-    team2 VARCHAR(50) NOT NULL,
-	round INTEGER NOT NULL,
-    start_time TIMESTAMP NOT NULL,
-	venue_id VARCHAR(100),
-	FOREIGN KEY (venue_id) REFERENCES venues(id)
+CREATE TABLE timezones (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	abbreviation VARCHAR(4),
+	utc_offset DECIMAL (4, 2) NOT NULL
 );
 
 CREATE TABLE venues (
@@ -29,11 +29,14 @@ CREATE TABLE venues (
 	FOREIGN KEY (timezone_id) REFERENCES timezones(id)
 );
 
-CREATE TABLE timezones (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(100),
-	abbreviation VARCHAR(4),
-	utc_offset DECIMAL (4, 2) NOT NULL,
-)
+CREATE TABLE matches (
+    match_id SERIAL PRIMARY KEY,
+    team1 VARCHAR(50) NOT NULL,
+    team2 VARCHAR(50) NOT NULL,
+	round INTEGER NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+	venue_id INTEGER,
+	FOREIGN KEY (venue_id) REFERENCES venues(id)
+);
 
 COMMIT TRANSACTION;
