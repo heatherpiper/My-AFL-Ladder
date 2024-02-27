9f858b2759564cd56176508d2abd674299f7cc78
Piper <heather.a.piper@gmail.com>
2024-02-26 20:41:25 -0500

    Fix percentage to only display to the hundreths

--- java/src/main/java/com/techelevator/service/WatchedGamesService.java
+++ java/src/main/java/com/techelevator/service/WatchedGamesService.java
@@ -7,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.List;
-import java.util.Collections;
 import java.util.Comparator;
 
 import com.techelevator.model.Game;
