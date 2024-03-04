8d61d0c1accd4ca681fdacb8fd9934ca089b7a45
Piper <heather.a.piper@gmail.com>
2024-03-04 14:26:34 -0500

    Implement resetLadder function in LadderService

--- vue/src/services/LadderService.js
+++ vue/src/services/LadderService.js
@@ -1,9 +1,15 @@
 import axios from 'axios';
 import store from '../store';
 
+const API_URL = 'http://localhost:9000';
+
 export default {
     getLadder() {
         const userId = store.state.user.id;
-        return axios.get(`http://localhost:9000/ladder/${userId}`);
+        return axios.get(`${API_URL}/ladder/${userId}`);
+    },
+    resetLadder() {
+        const userId = store.state.user.id;
+        return axios.post(`${API_URL}/ladder/${userId}/reset`);
     }
 };
