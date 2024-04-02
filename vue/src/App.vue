6afd41a9c3a644531cad1b2aeeda7de7b30c3847
Piper <heather.a.piper@gmail.com>
2024-04-01 20:19:46 -0400

    Adjust footer formatting to be more responsive

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -21,11 +21,11 @@
 
     <footer class="footer">
       <div class="footer-content">
-        <router-link to="/privacy-policy" class="footer-link">Privacy Policy</router-link>
-        <span>&middot;</span>
-        <router-link to="/terms-of-service" class="footer-link">Terms of Service</router-link>
-        <span>&middot;</span>
-        <a href="mailto:support@laterladder.com" class="footer-link">Contact Us</a>
+        <div class="footer-links">
+          <router-link to="/privacy-policy" class="footer-link">Privacy Policy</router-link>
+          <router-link to="/terms-of-service" class="footer-link">Terms of Service</router-link>
+          <a href="mailto:support@laterladder.com" class="footer-link">Contact Us</a>
+        </div>
         <div class="copyright">
           &copy; 2024 Later Ladder. All rights reserved.
         </div>
@@ -187,15 +187,30 @@ img {
   flex-shrink: 0;
   background-color: var(--afl-800);
   color: var(--afl-250);
-  padding: 2.5em 0 1em 0;
+  padding: 1.5em 0;
   text-align: center;
-  margin-top: 1.5em;
+  margin-top: 6em;
+}
+
+.footer-content {
+  font-size: 0.9em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.footer-links {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-bottom: 0.5em;
+  line-height: 1.25;
 }
 
 .footer-link {
   color: var(--afl-250);
   text-decoration: none;
-  margin: 0 0.5em;
+  margin: 0 0.75em;
 }
 
 .footer-link:hover {
@@ -208,7 +223,7 @@ img {
 
 .copyright {
   color: var(--afl-250);
-  margin-top: 1em;
+  margin-top: 0.5em;
 }
 
 @media (max-width: 600px) {
@@ -225,10 +240,13 @@ img {
     margin: 0 8px;
   }
 
-  .footer-content {
-    line-height: 0.5em;
-    font-size: 1em;
-    margin-top: 0.5em;
+  .footer {
+    margin-top: 1.5em;
+  }
+
+  .footer-links {
+    flex-direction: column;
+    align-items: center;
   }
 }
 
